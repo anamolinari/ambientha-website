ffda2d4dd038e4ba114d7802fb5c4af280f883e1
Ana Molinari <121121296+anamolinari@users.noreply.github.com>
2026-04-29 17:09:10 +0200

    Metadados Open Graph adicionado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,28 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Ambientha Decorações",
   description: "Referência e bom gosto para ambientes encantadores.",
+  openGraph: {
+    title: "Ambientha Decorações",
+    description: "Referência e bom gosto para ambientes encantadores.",
+    url: "https://ambientha.com.br",
+    siteName: "Ambientha",
+    images: [
+      {
+        url: "https://ambientha.com.br/images/open-graph-logo.png",
+        width: 1200,
+        height: 1200,
+        alt: "Ambientha Decorações",
+      },
+      {
+        url: "https://ambientha.com.br/images/open-graph.png",
+        width: 1200,
+        height: 630,
+        alt: "Ambientha Decorações",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
